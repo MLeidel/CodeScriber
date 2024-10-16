c5a2a77c4e2b33e5e3c9e4d8d0e1803077f9e90f
mleidel <michaelleidel321@gmail.com>
2024-10-16 10:52:18 -0500

    initial

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# CodeScriber
-Hack-able Desktop Code Editor
+## A _Hackable_ Desktop Code Editor for Linux
+
+__Uses "web app" technology via Python and Webview, Not Node and Electron.__
+
+_uses_ Python/Tkinter, pywebview/webview, html, CSS, javascript, and the Ace Code Editor (API).
+
+_See_ <a href='https://ace.c9.io/'>https://ace.c9.io</a> _for information on the Ace Code Editor._
+
+### Features:
+
+- Syntax highlighting
+- Auto indentation and outdent
+- An optional command line
+- Work with large documents (handles hundreds of thousands of lines without issue)
+- Fully customizable key bindings including vi and Emacs modes
+- Themes (TextMate themes can be imported)
+- Search and replace with regular expressions
+- Highlight matching parentheses
+- Toggle between soft tabs and real tabs
+- Displays hidden characters
+- Highlight selected word
+- Multiple cursor selection
+- Column select and edit mode
+- Customizable Keyword code snipits
+- Drag & Drop to open file
+- Initial language models:
+> _python, c, html, css, markdown, javascript, bash, sql, golang, json_  
+can be extended in the html code file
+
+### CodeScriber combines the following technologies:
+> - python (pywebview, markdown, tkinter)  
+- javascript (ace.js)  
+- css  
+- html  
+ 
---
+++ cs.py
@@ -0,0 +1,209 @@
+'''
+cs.py CodeScriber Code Editor
+Hak-able Desktop Code Editor
+Oct 2024 Michael Leidel
+'''
+
+import webview
+import sys, os
+import markdown
+import tkinter as tk
+import iniproc
+import subprocess
+from tkinter import filedialog
+
+p = os.path.realpath(__file__)
+p = os.path.dirname(p) + "/"
+# os.chdir(os.path.dirname(p))
+optionsFileName = p+"options.ini"
+lastFileName = p+"lastfile"
+
+opts = []
+opts = iniproc.read(optionsFileName,'width',
+                                   'height',
+                                   'terminal',
+                                   'filemanager',
+                                   'python',
+                                   'browser',
+                                   'startdir')
+
+current_file = ""  # tracks current file in use
+current_path = opts[6]  # tracks path
+
+# UNCOMMENT THIS FUNCTION TO WORK ON DUAL MONITORS (there is more)
+def window_coord():
+    px = window.x
+    py = window.y
+    geo = f"20x20+{px}+{py}"
+    return geo
+
+def find_file(filename, search_path):
+    ''' finds full-path of a file on Linux system '''
+    for root, dirs, files in os.walk(search_path):
+        if filename in files:
+            return os.path.join(root, filename)
+    return None
+
+def mdToHTML():
+    with open(current_file, 'r') as file:
+        htmlText = markdown.markdown(file.read())
+    htmlFile = current_file[:-3] + ".html"
+    # open in default browser
+    with open(htmlFile, 'w') as file:
+        file.write(htmlText)
+
+'''
+            connections to/from the HTML/Javascript
+'''
+class Api:
+
+    def onClose(self):
+        ''' immediate close no ask '''
+        with open(lastFileName, "w") as fout:
+            fout.write(current_file)
+        window.destroy()
+
+    def getFileName(self):
+        global current_file
+        return current_file
+
+    def open_file(self):
+        ''' called from javascript '''
+        global current_file, current_path
+        root = tk.Tk()  # COMMENT IF USING SINGLE MONITOR SYSTEM
+        root.geometry(window_coord())  # COMMENT IF USING SINGLE MONITOR SYSTEM
+        file_path = filedialog.askopenfilename(initialdir=current_path,
+                                               initialfile=os.path.basename(current_file),
+                                               title="Open file",
+                                               filetypes=(("all files", "*"),
+                                                         ("Python", "*.py"),
+                                                         ("C", "*.c"),
+                                                         ("h", "*.h"),
+                                                         ("HTML", "*.html"),
+                                                         ("CSS", "*.css")))
+        root.destroy()  # COMMENT IF USING SINGLE MONITOR SYSTEM
+        if file_path:
+            current_file = file_path
+            current_path = os.path.dirname(file_path)
+        else:
+            return ''
+
+        if file_path:
+            with open(current_file, 'r') as file:
+                return file.read()
+        return ''
+
+    def save_file(self, content):
+        ''' called from javascript '''
+        global current_file, current_path
+        root = tk.Tk()  # COMMENT IF USING SINGLE MONITOR SYSTEM
+        root.geometry(window_coord())  # COMMENT IF USING SINGLE MONITOR SYSTEM
+        file_path = filedialog.asksaveasfilename(initialdir=current_path,
+                                                 defaultextension=".txt",
+                                                 initialfile=os.path.basename(current_file),
+                                                 filetypes=(("all files", "*"),
+                                                            ("Python", "*.py"),
+                                                            ("C", "*.c"),
+                                                            ("h", "*.h"),
+                                                            ("HTML", "*.html"),
+                                                            ("CSS", "*.css")))
+        root.destroy()  # remove if single monitor
+        if file_path:
+            current_file = file_path
+            current_path = os.path.dirname(file_path)
+            if current_file.endswith(".md"):
+                mdToHTML()
+        else:
+            return ''
+
+        if file_path:
+            with open(file_path, 'w') as file:
+                file.write(content)
+            return current_file
+        else:
+            return ''
+
+    def quick_save_file(self, content):
+        global current_file, current_path
+        with open(current_file, 'w') as file:
+            file.write(content)
+        if current_file.endswith(".md"):
+            mdToHTML()
+        return current_file
+
+    def getCmdFile(self):
+        ''' Get the starting file from command line '''
+        global current_file, current_path
+        if current_file:
+            with open(current_file, 'r') as file:
+                return file.read()
+        return ''
+
+    def execPython(self):
+        global current_file, current_path
+        os.system(opts[4] + " " + current_file)
+
+    def execMarkdown(self):
+        global current_file, current_path
+        if not current_file.endswith(".md"):
+            return
+        # change to .html file
+        htmlFile = current_file[:-3] + ".html"
+        # open in default browser
+        subprocess.call([opts[5], htmlFile])
+
+    def execWebbrowser(self):
+        global current_file, current_path
+        subprocess.call([opts[5], current_file])
+
+    def execFileMgr(self):
+        global current_file, current_path
+        os.system(opts[3] + " " + current_path)
+
+    def execTerminal(self):
+        global current_file, current_path
+        print(opts[2] + current_path)
+        os.system(opts[2] + "=" +current_path)
+
+    def on_file_drop(self, file_name):
+        global current_file, current_path
+        current_file = find_file(file_name, "/home/")
+        print(f"file file: {current_file}")
+        current_path =  os.path.dirname(current_file)
+        print(f"file path: {current_path}")
+        if current_file:
+            with open(current_file, 'r') as file:
+                return file.read()
+        else:
+            return ''
+
+
+#  END OF JS_API CLASS
+
+if __name__ == '__main__':
+    api = Api()
+
+    w = int(opts[0])  # width
+    h = int(opts[1])  # height
+
+    '''
+    The following opens a file from the command line
+    or by default the last file used at shut down.
+    '''
+    if len(sys.argv) > 1:
+        current_file = sys.argv[1]
+        current_path = os.path.realpath(current_file)
+        #current_path =  os.path.dirname(sys.argv[1])
+    else:
+        if os.path.isfile(lastFileName):
+            with open(lastFileName, 'r') as file:
+                current_file = file.readline().strip()
+                current_path =  os.path.dirname(current_file)
+
+    window = webview.create_window('CodeScriber Code Editor',
+                     url='cs.html',
+                     width=w,
+                     height=h,
+                     js_api=api)
+
+    webview.start()
